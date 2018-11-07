c8983c789e5f3403ea7f882fd15368c4ba042583
(Jeremy Peyrat) prstoad@gmail.com <prstoad@gmail.com>
2018-11-07 15:53:33 +0100

    done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  resources :restaurants do
+    resources :reviews, only: [ :index, :new, :create]
+    # resources :reviews, only: [ :show, :edit, :update, :destroy ]
+    collection do                       # collection => no restaurant id in URL
+      get 'review', to: "restaurants#review"  # RestaurantsController#top
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
